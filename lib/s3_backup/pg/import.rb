e83cece4c602801924a75ee3a921a56a62aa6340
Keith Broughton <keith.broughton@fatzebra.com.au>
2018-06-19 14:28:38 +1000

    Download and uncompress DB before drop

--- lib/s3_backup/pg/import.rb
+++ lib/s3_backup/pg/import.rb
@@ -13,11 +13,11 @@ module S3Backup
       end
 
       def now!
-        puts 'Setup local database ...'
-        setup_local_database
         puts 'Downloading pg database ...'
         S3Backup::Storage::S3.new.download!(pg_database_name, Config.s3_pg_path, pg_dump_s3_file.path)
         umcompress_file
+        puts 'Setup local database ...'
+        setup_local_database
         puts "Loading data in #{database} ..."
         load_file
         clean_env
